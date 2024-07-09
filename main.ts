b456a184f2ae296cc4be561d5b7a40631eb93cba
Michael Kummer <kummermi@gmail.com>
2024-07-09 15:55:07 +0200

    ft

--- main.ts
+++ main.ts
@@ -143,6 +143,8 @@ namespace nanoMedForLife {
             lastReceivedNumber = advancerSpeed;
             dataReceived = true;
            // lastReceivedTime = control.millis();
+            basic.showNumber(advancerSpeed)
+            basic.showNumber(+ dataReceived)
             actualCallback(advancerSpeed);
         });
     }
